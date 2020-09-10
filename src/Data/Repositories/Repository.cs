3e1e636a5555c0e37ba6844f324a2acb158d75b9
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2020-09-10 15:25:10 -0300

    Create interfaces and classes for IRepository and IUnitOfWork

---
+++ src/Data/Repositories/Repository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext _db;
+
+        public Repository(DbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _db.Set<TEntity>().AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            await _db.Set<TEntity>().AddRangeAsync(entities);
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _db.Set<TEntity>().Where(predicate);
+        }
+
+        public async Task<IEnumerable<TEntity>> FindAllAsync()
+        {
+            return await _db.Set<TEntity>().ToListAsync();
+        }
+
+        public ValueTask<TEntity> FindByIdAsync(int id)
+        {
+            return _db.Set<TEntity>().FindAsync(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _db.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _db.Set<TEntity>().RemoveRange(entities);
+        }
+
+        public async Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _db.Set<TEntity>().SingleOrDefaultAsync(predicate);
+        }
+    }
+}
---
+++ src/Data/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core;
+
+namespace Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ImperiusDbContext _context;
+
+        public UnitOfWork(ImperiusDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
